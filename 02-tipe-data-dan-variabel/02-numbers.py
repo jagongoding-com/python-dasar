2caaf58007dfc1d8589a0ba3c6d352ab376fb9cf
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-07 12:38:13 +0700

    tipe data numbers and strings

---
+++ 02-tipe-data-dan-variabel/02-numbers.py
@@ -0,0 +1,20 @@
+panjang = 5
+lebar = 10.5
+luas = panjang * lebar
+
+print(panjang, '*', lebar, '=', luas)
+print("Tipe dari variabel panjang:", type(panjang))
+print("Tipe dari variabel lebar:", type(lebar))
+print("Tipe dari variabel luas:", type(luas))
+
+
+print()
+
+a = 5j
+b = 10j
+c = a + b
+
+print(a, '+', b, '=', c)
+print('Tipe dari a:', type(a))
+print('Tipe dari b:', type(b))
+print('Tipe dari c:', type(c))
---
+++ 02-tipe-data-dan-variabel/03-string.py
@@ -0,0 +1,13 @@
+nama_depan = "Wahit"
+nama_belakang = 'Abdulloh'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '12'
+alamat = 'Bangkalan'
+kata_mutiara = "Don't judge a book by it's cover"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
