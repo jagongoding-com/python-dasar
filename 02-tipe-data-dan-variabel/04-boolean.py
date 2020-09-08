73815e1c697fd5aab0fa4610c657879cbe2a7bb4
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-08 12:44:14 +0700

    tipe data boolean

---
+++ 02-tipe-data-dan-variabel/04-boolean.py
@@ -0,0 +1,7 @@
+saya_orang_indonesia = True
+saya_adalah_robot = False
+
+print('Apakah saya orang Indonesia?', saya_orang_indonesia)
+print('Apakah saya adalah robot?', saya_adalah_robot)
+print('Tipe dari saya_orang_indonesia', type(saya_orang_indonesia))
+print('Tipe dari saya_adalah_robot', type(saya_adalah_robot))
