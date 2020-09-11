16d191078ccddb893037fd16088aca81c1bfecd4
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-11 09:57:42 +0700

    tipe data dan variabel

--- 02-tipe-data-dan-variabel/01-overview.py
+++ 02-tipe-data-dan-variabel/01-overview.py
@@ -5,3 +5,18 @@ sudah_menikah = True
 print('nama:', nama)
 print('ujia:', usia)
 print('sudah menikah:', sudah_menikah)
+
+a, b, c = 1, 2, "Mantap"
+
+print('a:', a)
+print('b:', b)
+print('c:', c)
+
+# kita juga bisa memberikan satu nilai yang sama untuk
+# beberapa variabel
+
+d = e = f = 10
+
+print('d:', d)
+print('e:', e)
+print('f:', f)
