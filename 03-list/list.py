fadf149e0eaae3377b561f0f09a062c22dc458aa
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-12 09:58:02 +0700

    list overview

---
+++ 03-list/list.py
@@ -0,0 +1,30 @@
+# list kosong
+list_kosong = []
+
+# list yang berisi kumpulan string
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+# list yang berisi kumpulan integer
+list_nilai = [80, 70, 90, 60]
+
+# list campuran
+list_jawaban = [150, 33.33, 'Presiden Sukarno', False]
+
+print('list_kosong:', list_kosong)
+print('list_buah:', list_buah)
+print('list_nilai:', list_nilai)
+print('list_jawaban:', list_jawaban)
+
+print('----------')
+
+print(list_buah[0])
+print(list_buah[2])
+print(list_buah[1])
+print(list_buah[3])
+
+print('----------')
+
+print(list_buah[-1])
+print(list_buah[-2])
+print(list_buah[-3])
+print(list_buah[-4])
