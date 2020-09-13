090225ebe090f333ec4bc2f93391b9d82ade9ed3
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-13 10:08:59 +0700

    slicing list

---
+++ 03-list/02-slicing.py
@@ -0,0 +1,22 @@
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+print(list_buah[0:1])
+print(list_buah[0:2])
+print(list_buah[1:3])
+print(list_buah[0:-1])
+print(list_buah[-1:-3])
+print(list_buah[-1:3])
+print(list_buah[-3:-1])
+
+print('---------')
+print(list_buah[0:])
+print(list_buah[1:])
+print(list_buah[2:])
+print(list_buah[3:])
+print(list_buah[:0])
+print(list_buah[:1])
+print(list_buah[:2])
+print(list_buah[:3])
+print(list_buah[:4])
+
+
