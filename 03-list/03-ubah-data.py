3c1b9591818748723fb4781c516e599bfa20430b
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-14 10:28:51 +0700

    ubah data list

---
+++ 03-list/03-ubah-data.py
@@ -0,0 +1,18 @@
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+print(list_buah)
+
+# ubah data pertama
+list_buah[0] = 'Jeruk'
+
+print(list_buah)
+
+# ubah data terakhir
+list_buah[-1] = 'Mangga'
+
+print(list_buah)
+
+# ubah data dalam range
+list_buah[1:3] = ['Naga', 'Pepaya']
+
+print(list_buah)
