a84e9f44c479ed64ec8e792119d8ea57bc563a46
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-18 16:00:15 +0700

    list: mengurutkan data

---
+++ 03-list/05-mengurutkan-data.py
@@ -0,0 +1,10 @@
+list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian']
+print(list_buah)
+
+# urutkan secara ascending
+list_buah.sort()
+print(list_buah)
+
+# membalikkan posisi item list (tidak harus berurut)
+list_buah.reverse()
+print(list_buah)
