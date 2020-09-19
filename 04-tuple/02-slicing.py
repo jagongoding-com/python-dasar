b425384bbd7809250e85732144c3626c40c4877b
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-19 19:54:24 +0700

    tuple slicing

---
+++ 04-tuple/02-slicing.py
@@ -0,0 +1,22 @@
+tuple_buah = ('Pisang', 'Nanas', 'Melon', 'Durian')
+
+print(tuple_buah[0:1])
+print(tuple_buah[0:2])
+print(tuple_buah[1:3])
+print(tuple_buah[0:-1])
+print(tuple_buah[-1:-3])
+print(tuple_buah[-1:3])
+print(tuple_buah[-3:-1])
+
+print('\n', '-' * 5, '\n')
+
+
+print(tuple_buah[0:])
+print(tuple_buah[1:])
+print(tuple_buah[2:])
+print(tuple_buah[3:])
+print(tuple_buah[:0])
+print(tuple_buah[:1])
+print(tuple_buah[:2])
+print(tuple_buah[:3])
+print(tuple_buah[:4])
