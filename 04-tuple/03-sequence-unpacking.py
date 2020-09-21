f9dd9c7eb150792534bc839aa595db68c2ab1157
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-21 20:12:40 +0700

    tuple sequence unpacking

---
+++ 04-tuple/03-sequence-unpacking.py
@@ -0,0 +1,10 @@
+siswa = ('Nurul Huda', 'Bangkalan', 24)
+
+# ekstrak data atau juga dinamakan sequence unpacking
+nama, asal, usia = siswa
+
+# setiap variabel di atas akan memiliki nilai dari tiap isi tuple
+# secara berurutan
+print('Nama:', nama)
+print('Asal:', asal)
+print('Usia:', usia)
