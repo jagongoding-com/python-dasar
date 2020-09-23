59044d4cbf7ad8c579bdf188f4f33bcd262dd1da
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-23 21:56:35 +0700

    operator perbandingan

---
+++ 05-operator/02-perbandingan.py
@@ -0,0 +1,8 @@
+a, b = 5, 10
+
+print(a, '>', b, '=', a > b)
+print(a, '<', b, '=', a < b)
+print(a, '==', b, '=', a == b)
+print(a, '!=', b, '=', a != b)
+print(a, '>=', b, '=', a >= b)
+print(a, '<=', b, '=', a <= b)
