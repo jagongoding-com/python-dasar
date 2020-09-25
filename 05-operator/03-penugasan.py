f4e1458ffc67586ba199c9ed1b047935ed7e6e31
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-09-25 22:25:28 +0700

    operator penugasan

---
+++ 05-operator/03-penugasan.py
@@ -0,0 +1,43 @@
+# penugasan pertama
+a = 10
+print('a = 10 -> ', a)
+
+a += 5
+print('a += 5 -> ', a)
+
+a -= 3
+print('a -= 3 -> ', a)
+
+a *= 6
+print('a *= 6 -> ', a)
+
+a /= 8
+print('a /= 8 -> ', a)
+
+# karena a jadi float, kita ubah lagi menjadi integer
+a = int(a)
+
+a %= 9
+print('a %= 9 -> ', a)
+
+a //= 6
+print('a //= 6 -> ', a)
+
+a **= 1
+print('a **= 1 -> ', a)
+
+
+a &= 2
+print('a &= 2 -> ', a)
+
+a |= 3
+print('a |= 3 -> ', a)
+
+a ^= 4
+print('a ^= 4 -> ', a)
+
+a >>= 4
+print('a >>= 4 -> ', a)
+
+a <<= 2
+print('a <<= 4 -> ', a)
---
+++ 05-operator/04-logika.py
@@ -0,0 +1,8 @@
+print(True and True)
+print(1 + 2 == 3 and True)
+print('----')
+print(False or 1 > 5)
+print(False or 5 > 2)
+print('----')
+print(not(1 > 5))
+print(not(1 < 5))
