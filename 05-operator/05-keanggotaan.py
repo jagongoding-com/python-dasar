3bf175ac950f6b12bbf5dabaa19cc575ceda2827
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-10-01 15:48:12 +0700

    python operator keanggotaan

---
+++ 05-operator/05-keanggotaan.py
@@ -0,0 +1,16 @@
+perusahaan = 'Microsoft'
+list_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+
+# ini adalah dictionary, insyaallah akan kita pelajari
+# di pertemuan-pertemuan yang akan datang
+mahasiswa = {
+  'nama': 'Lendis Fabri',
+  'asal': 'Lamongan'
+}
+
+print("Apakah 'c' ada di variabel perusahaan?", 'c' in perusahaan)
+print("Apakah 'z' tidak ada di variabel perusahaan?", 'c' not in perusahaan)
+print("Apakah 'Madura' ada di variabel list_pulau?", 'Madura' in perusahaan)
+print("Apakah 'Madura' tidak ada di variabel list_pulau?", 'Madura' not in perusahaan)
+print("Apakah atribut 'nama' ada di variabel mahasiswa?", 'nama' in mahasiswa)
+
