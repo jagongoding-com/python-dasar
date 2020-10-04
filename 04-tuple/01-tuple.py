8f5be3d7a0dd0ca4b58e1367b789878b17f0b924
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-10-04 19:15:01 +0700

    tuple awal

---
+++ 04-tuple/01-tuple.py
@@ -0,0 +1,20 @@
+# cara standar
+tuple_jenis_kelamin = ('laki-laki', 'perempuan')
+
+# tanpa kurung
+tuple_status_perkawinan = 'menikah', 'lajang'
+
+# menggunakan fungsi tuple()
+tuple_lulus = tuple(['lulus', 'tidak lulus'])
+
+# tuple kosong
+tuple_kosong = ()
+
+# tuple tunggal
+tuple_tunggal = (10,)
+
+print(tuple_jenis_kelamin[1])
+print(tuple_jenis_kelamin[0])
+
+print(tuple_jenis_kelamin[-2])
+print(tuple_jenis_kelamin[-1])
